1395a510e939e2bfd73740f1dc06f99d86cc7a0f
wei_li <wei_li@dynamicrisk.net>
2025-03-07 13:34:51 -0700

    Add API versioning and update JointLength endpoint definition

--- src/EndpointDefinitions/JointLengthEndpointDefinition.cs
+++ src/EndpointDefinitions/JointLengthEndpointDefinition.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace JointLengthSequencing.EndpointDefinitions;
 
 /// <summary>
@@ -5,47 +7,63 @@ namespace JointLengthSequencing.EndpointDefinitions;
 /// </summary>
 public class JointLengthEndpointDefinition : IEndpointDefinition
 {
-	/// <summary>
-	/// Defines the endpoints.
-	/// </summary>
-	/// <param name="app">The app.</param>
-	/// <param name="evnt">The environment.</param>
-	public void DefineEndpoints(WebApplication app, IWebHostEnvironment evnt)
-	{
-		app.MapPost("api/JointLength", AlignAsync);
-	}
+    /// <summary>
+    /// Defines the endpoints.
+    /// </summary>
+    /// <param name="app">The app.</param>
+    /// <param name="evnt">The environment.</param>
+    public void DefineEndpoints(WebApplication app, IWebHostEnvironment evnt)
+    {
+        app.MapPost("/api/v{version:apiVersion}/JointLength", AlignAsync)
+            .WithMetadata(
+                new ApiVersionAttribute("1.0"),
+                new ApiVersionAttribute("2.0"),
+                new ApiVersionAttribute("3.0")
+            );
+    }
 
-	/// <summary>
-	/// Configures the necessary services for joint length sequencing.
-	/// </summary>
-	/// <param name="services">The service collection to which the services are added.</param>
-	public void DefineServices(IServiceCollection services)
-	{
-		services.AddSingleton<IJointLengthSequencer, JointLengthSequencer3>();
-	}
+    /// <summary>
+    /// Configures the necessary services for joint length sequencing.
+    /// </summary>
+    /// <param name="services">The service collection to which the services are added.</param>
+    public void DefineServices(IServiceCollection services)
+    {
+        services.AddSingleton<JointLengthSequencer>();
+        services.AddSingleton<JointLengthSequencer2>();
+        services.AddSingleton<JointLengthSequencer3>();
+    }
 
-	/// <summary>
-	/// Aligns two datasets using the joint length sequencing algorithm.
-	/// </summary>
-	/// <param name="jointLengthSequencer">The joint length sequencer.</param>
-	/// <param name="alignmentData">The alignment data.</param>
-	/// <returns>The list of aligned points.</returns>
-	private static async Task<IResult> AlignAsync(IJointLengthSequencer jointLengthSequencer, AlignmentDataRequest alignmentData)
-	{
-		var errors = new List<string>();
-		if (!alignmentData.Validate(ref errors))
-		{
-			Results.BadRequest(string.Join("\n", errors!));
-		}
+    /// <summary>
+    /// Aligns two datasets using the joint length sequencing algorithm.
+    /// </summary>
+    /// <param name="version">The version.</param>
+    /// <param name="serviceProvider">The service provider.</param>
+    /// <param name="alignmentData">The alignment data.</param>
+    /// <returns>A Task of type IResult</returns>
+    private static async Task<IResult> AlignAsync(ApiVersion version, IServiceProvider serviceProvider, AlignmentDataRequest alignmentData)
+    {
+        var errors = new List<string>();
+        if (!alignmentData.Validate(ref errors))
+        {
+            Results.BadRequest(string.Join("\n", errors!));
+        }
 
-		var forecasts = await jointLengthSequencer.CalculateMatches(
-			alignmentData.BaseData,
-			alignmentData.TargetData,
-			alignmentData.PivotPercentile,
-			alignmentData.Tolerance,
-			alignmentData.PivotRequired,
-			alignmentData.BaseLengthCol,
-			alignmentData.TargetLengthCol);
-		return Results.Ok(forecasts);
-	}
+        IJointLengthSequencer jointLengthSequencer = version.MajorVersion switch
+        {
+            1 => serviceProvider.GetRequiredService<JointLengthSequencer>(),
+            2 => serviceProvider.GetRequiredService<JointLengthSequencer2>(),
+            3 => serviceProvider.GetRequiredService<JointLengthSequencer3>(),
+            _ => throw new NotImplementedException()
+        };
+
+        var forecasts = await jointLengthSequencer.CalculateMatches(
+            alignmentData.BaseData,
+            alignmentData.TargetData,
+            alignmentData.PivotPercentile,
+            alignmentData.Tolerance,
+            alignmentData.PivotRequired,
+            alignmentData.BaseLengthCol,
+            alignmentData.TargetLengthCol);
+        return Results.Ok(forecasts);
+    }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,22 @@
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointDefinitions(typeof(IEndpointDefinition));
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
 	options.SerializerOptions.NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals;
+});
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new UrlSegmentApiVersionReader(),
+        new HeaderApiVersionReader("X-API-Version")
+    );
 });
 
 var app = builder.Build();
